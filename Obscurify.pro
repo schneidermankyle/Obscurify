fbc24aad7157c9dd1d3f6d54c81d616a8679f42c
Kyle Schneiderman <schneidermankyle@gmail.com>
2016-05-16 16:28:55 -0700

    V 1.0.0
    Version 1.0.0 supporting Javascript files.

--- Obscurify.pro
+++ Obscurify.pro
@@ -9,9 +9,9 @@ QT       += core gui
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
 CONFIG += c++11
+
 TARGET = Obscurify
 TEMPLATE = app
-
 
 SOURCES += main.cpp\
         mainwindow.cpp
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,9 +3,7 @@
 #include <qfiledialog>
 #include <QMessageBox>
 #include <fstream>
-#include <vector>
 #include <string>
-
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
@@ -37,11 +35,11 @@ void MainWindow::on_continue_2_clicked()
     // Create a string to hold our new content
     QString content = "";
     std::string path = ui->lineEdit->text().toStdString().substr(0, ui->lineEdit->text().toStdString().find_last_of('/')+1);
-    std::string fileName = ui->lineEdit->text().toStdString().substr(ui->lineEdit->text().toStdString().find_last_of('/')+1, ui->lineEdit->text().toStdString().find_last_of('.')+1);
-    bool keepSpaces = false;
-    bool ignoreAll = false;
+    std::string fileName = ui->lineEdit->text().toStdString().substr(ui->lineEdit->text().toStdString().find_last_of('/')+1, ui->lineEdit->text().toStdString().find_last_of('.') - ui->lineEdit->text().toStdString().find_last_of('/')-1);
+    std::string extension = ui->lineEdit->text().toStdString().substr(ui->lineEdit->text().toStdString().find_last_of('.'));
+    bool keepSpaces = false, ignoreAll = false;
     char buffer;
-    int commentType = 0;
+    int commentType = 0, count = 0;
 
     // Create a new file stream
     std::fstream fs;
@@ -68,16 +66,16 @@ void MainWindow::on_continue_2_clicked()
             }
 
             // Is this a quote? if so we should preserve string integrity
-            if (keepSpaces == false && (holder == '"' || holder == 39) ) {
+            if (keepSpaces == false && (buffer == '"' || buffer == '\'') ) {
                 keepSpaces = true;
-            } else if (keepSpaces == true && (holder == '"' || holder == 39)) {
+            } else if (keepSpaces == true && (buffer == '"' || buffer == '\'')) {
                 keepSpaces = false;
             }
 
             //Now we determine if we want to do anything with this actual character
-            if (!ignoreAll) {
+            if (!ignoreAll && count > 0) {
                 if (!keepSpaces){
-                    if (buffer != ' ' && buffer != '\n')
+                    if (buffer != ' ' && buffer != '\n' && buffer != '\t')
                         content += buffer;
                 } else {
                     if (buffer != '\n')
@@ -86,14 +84,16 @@ void MainWindow::on_continue_2_clicked()
             }
 
             buffer = holder;
+            count++;
         }
         fs.close();
 
         // Now time to create the new file
+        std::ofstream test(path + fileName + "-min" + extension, std::ios::out);
+        if (test << content.toStdString())
+            QMessageBox::information(this, tr("Successful"), "Success, your minified file was written into the same directory of your original file.");
 
     } else {
         QMessageBox::critical(this, tr("Error"), "This file could not be opened, please select a different file or make sure the proper privlidges are assigned to this user.");
     }
-
-    QMessageBox::information(this, tr("Succes"), content);
 }
